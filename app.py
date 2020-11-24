c52111c79396de59757b9f5a2f49eab06f95f703
Brian Young <Brian.Young@sacw.com>
2020-11-24 12:40:56 -0600

    flask maintenance

--- app.py
+++ app.py
@@ -10,8 +10,6 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect
 import datetime as dt
 
-from ipynb.fs.full.climate_analysis import *
-
 app = Flask(__name__)
 
 from app import app
@@ -30,16 +28,45 @@ def index():
 
 @app.route("/api/v1.0/precipitation")
 def precip():
+    engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+    Base = automap_base()
+    Base.prepare(engine, reflect=True)
+    Measurement = Base.classes.measurement
+    session = Session(engine)
+
+    lastDate = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
+    lastDateDT = dt.date.fromisoformat(lastDate)
+    year_query_date = lastDateDT - dt.timedelta(days=365)
+    year_query_date
+
+    precip_query = session.query(Measurement.date, Measurement.prcp).\
+                filter(Measurement.date >= year_query_date).all()
+
     date_list = [element[0] for element in precip_query]
     precip_list = [element[1] for element in precip_query]
     precip_dict = dict(zip(date_list, precip_list))
     
+    session.close()
     return jsonify(precip_dict)
 
 @app.route("/api/v1.0/stations")
 def stations():
+    engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+    Base = automap_base()
+    Base.prepare(engine, reflect=True)
+    Measurement = Base.classes.measurement
+    session = Session(engine)
 
-    return jsonify(stations_df['Station'])
+    lastDate = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
+    lastDateDT = dt.date.fromisoformat(lastDate)
+    year_query_date = lastDateDT - dt.timedelta(days=365)
+    year_query_date
+
+    station_query = session.query(Measurement.station).group_by(Measurement.station).\
+                filter(Measurement.date >= year_query_date).all()
+
+    session.close()
+    return jsonify(station_query)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
@@ -47,20 +74,27 @@ def tobs():
     Base = automap_base()
     Base.prepare(engine, reflect=True)
     Measurement = Base.classes.measurement
+    session = Session(engine)
+
+    lastDate = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
+    lastDateDT = dt.date.fromisoformat(lastDate)
+    year_query_date = lastDateDT - dt.timedelta(days=365)
+    year_query_date
 
 
     tobs_query = session.query(Measurement.station, Measurement.date, Measurement.tobs).\
-                filter(Measurement.station == stations_df['Station'][0]).\
+                filter(Measurement.station == 'USC00519281').\
                 filter(Measurement.date >= year_query_date).all()
     session.close()
     return jsonify(tobs_query)
 
-@app.route("/api/v1.0/<start>")
+@app.route("/api/v1.0/<int:start>")
 def start(start):
     engine = create_engine("sqlite:///Resources/hawaii.sqlite")
     Base = automap_base()
     Base.prepare(engine, reflect=True)
     Measurement = Base.classes.measurement
+    session = Session(engine)
 
     start_date = dt.date(start)
 
@@ -74,12 +108,13 @@ def start(start):
     session.close()
     return jsonify(TMIN, TMAX, TAVG)
 
-@app.route("/api/v1.0/<start>/<end>")
+@app.route("/api/v1.0/<int:start>/<int:end>")
 def duration(start, end):
     engine = create_engine("sqlite:///Resources/hawaii.sqlite")
     Base = automap_base()
     Base.prepare(engine, reflect=True)
     Measurement = Base.classes.measurement
+    session = Session(engine)
 
     start_date = dt.date(start).strftime('%Y-%m-%d')
     end_date = dt.date(end).strftime('%Y-%m-%d')
